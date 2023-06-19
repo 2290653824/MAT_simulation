5fd19b48776917143dc14f2068120a5ec75532a0
BabyJIAN <2290653824@qq.com>
2023-06-19 10:21:07 +0800

    init rep

---
+++ src/main/java/org/zj/Main.java
@@ -0,0 +1,11 @@
+package org.zj;
+
+/**
+ * @author zhengjian
+ * @date 2023-06-19 10:13
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
